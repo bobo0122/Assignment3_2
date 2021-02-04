bfd660a3c1a45bdcc6679aed017b212c0d76c8fb
bobo0122 <35634479+bobo0122@users.noreply.github.com>
2021-02-03 17:38:18 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Assignment3_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult EnterMovie()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovie(ApplicationResponse appResponse)
+        //this is just going to take you to the DatingApplication.cshtml
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddApplication(appResponse);
+                return View("Confirmation",appResponse);
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+
+
+        public IActionResult MoviesList()
+        {
+           return View(TempStorage.Applications.Where(r => r.Title != "Independence Day"));
+            
+        }
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+
+      
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ApplicationResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3_2.Models
+{
+    public class ApplicationResponse
+    {
+
+        [Required]
+        public string Category { get; set; }
+
+        //prop tab tab builds the following:
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string SelectRatingType { get; set; }
+        public bool? Edited { get; set; }
+        public string LentTo { get; set; }
+
+        [StringLength(25)]
+        public string Notes { get; set; }
+    }
+}
